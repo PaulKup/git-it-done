687f54adddd3f564defe05da180b9506dcb86ddc
ChillyWilly <noreply>
2021-01-27 02:21:38 -0500

    error handling for repo fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,20 +3,32 @@ var nameInputEl = document.querySelector("#username");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 var repoContainerEl = document.querySelector("#repos-container");
 
-var getUserRepos = function(user) {
-//format the github api url
-var apiUrl = "https://api.github.com/users/" + user + "/repos";
-// fetch expexts a promise so the return value goes directly into the then function argument regardless or arg name. 
-// same for then also will directly put return info into a promise function argument
-    fetch(apiUrl).then(function(response){
-        response.json().then(function(data) {
-            displayRepos(data, user);
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    // fetch expexts a promise so the return value goes directly into the then function argument regardless or arg name. 
+    // same for then also will directly put return info into a promise function argument
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data, user);
+                });
+            } else {
+                alert("Error: " + response.status);
+            }
         })
-    });
-
+        // .catch is the fetch api's way of handling network errors
+        .catch(function(error) {
+            alert("Unable to connect to GitHub");
+        });
 };
 
-var displayRepos = function(repos, searchTerm) {
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
 
@@ -26,21 +38,23 @@ var displayRepos = function(repos, searchTerm) {
         var titleEl = document.createElement("span");
         titleEl.textContent = repos[i].full_name;
         var statusEl = document.createElement("span");
-        statusEl.classList = "flex-row align-center"; 
+        statusEl.classList = "flex-row align-center";
+
         if (repos[i].open_issues_count > 0) {
             statusEl.innerHTML =
-            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+                "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
         } else {
             statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
         }
+
         repoEl.appendChild(titleEl);
         repoEl.appendChild(statusEl);
         repoContainerEl.appendChild(repoEl);
     }
-    
+
 }
 
-var formSubmitHandler = function(event) {
+var formSubmitHandler = function (event) {
     event.preventDefault();
     var username = nameInputEl.value.trim();
 
